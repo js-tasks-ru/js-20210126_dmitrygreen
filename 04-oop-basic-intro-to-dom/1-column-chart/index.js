331c24e4819660df403789e1f3cc859e2553c38a
Dmitry Green <dmitryblazhyyevskyy@gmail.com>
2021-02-13 10:38:35 +0200

    задача 3го модуля. решил частично. тесты не проходит

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,58 @@
-export default class ColumnChart {
+function chartTemplate(label, value, columns, link) {
+  return `
+    <div class="column-chart" style="--chart-height: 50">
+      <div class="column-chart__title">
+        Total ${label}
+        ${link == '#' ? `<a href="${link}" class="column-chart__link">View all</a>` : ''}
+      </div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">${value}</div>
+        <div data-element="body" class="column-chart__chart">
+          ${columns}
+        </div>
+      </div>
+    </div>
+  `;
+}
 
+function columnsTemplate(data) {
+  let columns = '';
+  data && data.forEach( (item) => {
+   columns += `<div style="--value: ${Math.round(item/2)}" data-tooltip="${item}%"></div>`; }
+  )
+  return columns;
+}
+
+export default class ColumnChart {
+  constructor({data = '', label = orders, value = 344, link = '#'} = {}) {
+    this.data = data;
+    this.label = label;
+    this.value = value;
+    this.link = link;
+    this.container = null;
+    this.element = null;
+  
+    this.render()
+  }
+
+  render() {
+    const columns = columnsTemplate(this.data);
+    const chart = chartTemplate( this.label, this.value, columns, this.link);
+
+    this.container = document.createElement('div');
+    this.container.innerHTML = chart;
+    this.element = this.container.firstElementChild;
+  }
+
+  get element() {
+    return this.element;
+  }
+
+  update() {
+    this.render();
+  }
+
+  remove() {
+    this.element.remove();
+  }
 }
