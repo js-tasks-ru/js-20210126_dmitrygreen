cbe9553f680fff38fcbeee5d04e3cfa0df8226f5
Dmitry Green <dmitryblazhyyevskyy@gmail.com>
2021-02-15 15:54:22 +0200

    задача 3го модуля. переписал

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,58 +1,99 @@
-function chartTemplate(label, value, columns, link) {
-  return `
-    <div class="column-chart" style="--chart-height: 50">
-      <div class="column-chart__title">
-        Total ${label}
-        ${link == '#' ? `<a href="${link}" class="column-chart__link">View all</a>` : ''}
-      </div>
-      <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">${value}</div>
-        <div data-element="body" class="column-chart__chart">
-          ${columns}
-        </div>
-      </div>
-    </div>
-  `;
-}
-
-function columnsTemplate(data) {
-  let columns = '';
-  data && data.forEach( (item) => {
-   columns += `<div style="--value: ${Math.round(item/2)}" data-tooltip="${item}%"></div>`; }
-  )
-  return columns;
-}
-
 export default class ColumnChart {
-  constructor({data = '', label = orders, value = 344, link = '#'} = {}) {
+  subElements = {};
+  chartHeight = 50;
+
+  constructor({
+    data = [],
+    label = '',
+    link = '',
+    value = 0
+  } = {}) {
     this.data = data;
     this.label = label;
-    this.value = value;
     this.link = link;
-    this.container = null;
-    this.element = null;
-  
-    this.render()
-  }
+    this.value = value;
 
-  render() {
-    const columns = columnsTemplate(this.data);
-    const chart = chartTemplate( this.label, this.value, columns, this.link);
-
-    this.container = document.createElement('div');
-    this.container.innerHTML = chart;
-    this.element = this.container.firstElementChild;
-  }
-
-  get element() {
-    return this.element;
-  }
-
-  update() {
     this.render();
   }
 
-  remove() {
+  getColumnBody(data) {
+    const maxValue = Math.max(...data);
+    const scale = this.chartHeight / maxValue;
+
+    return data
+      .map(item => {
+        const percent = (item / maxValue * 100).toFixed(0);
+
+        return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+      })
+      .join('');
+  }
+
+  getLink() {
+    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+  }
+
+  get template() {
+    return `
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          Total ${this.label}
+          ${this.getLink()}
+        </div>
+        <div class="column-chart__container">
+           <div data-element="header" class="column-chart__header">
+             ${this.value}
+           </div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.getColumnBody(this.data)}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+
+    if (this.data.length) {
+      this.element.classList.remove('column-chart_loading');
+    }
+
+    this.subElements = this.getSubElements(this.element);
+    console.log(this.subElements);
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElement) => {
+      
+      accum[subElement.dataset.element] = subElement;
+
+      console.log(subElement);
+      
+      console.log(subElement.dataset.element);
+      console.log(accum);
+
+      return accum;
+    }, {});
+  }
+
+  update(data) {
+    this.subElements.body.innerHTML = this.getColumnBody(data);
+  }
+
+  remove () {
     this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
   }
 }
